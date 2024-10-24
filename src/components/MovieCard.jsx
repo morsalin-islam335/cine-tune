0f5c78b3d6d557380668a58ec1d72e863c6c6092
morsalin-islam335 <weshallovercome335@gmail.com>
2024-10-25 00:43:05 +0600

    Open Modal after clicking a single movie implemented

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,32 @@
 /* eslint-disable react/prop-types */
 
 import { getImageUrl } from "../utils/cine-utils";
+import CardModals from "./CardModals";
 import Star from "./Star";
+
+import { useState } from "react";
 export default function MovieCard({ movie }) {
+  const [showDetailsModal, setShowModals] = useState(false);
+  const handleShowModal = () => {
+    setShowModals(true);
+  };
+
+  const handleClose = () => {
+    setShowModals(false);
+  };
   return (
     <>
+      {showDetailsModal && (
+        <CardModals movie={movie} onCloseClick={handleClose} />
+      )}
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-        <img
-          className="w-full object-cover"
-          src={getImageUrl(movie.cover)}
-          alt=""
-        />
+        <a href="#" onClick={handleShowModal}>
+          <img
+            className="w-full object-cover"
+            src={getImageUrl(movie.cover)}
+            alt=""
+          />
+        </a>
         <figcaption className="pt-4">
           <h3 className="text-xl mb-1">{movie.title}</h3>
           <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
